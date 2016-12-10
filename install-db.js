49b54332f287b9adef5209a64b58ba567daf1762
vigneshj6 <vigneshj6@gmail.com>
2016-12-10 07:30:50 +0000

    initializing database

--- install-db.js
+++ install-db.js
@@ -1,4 +1,15 @@
 var db = require('./routes/postgresql');
 console.log('configuring History DataBase ... ');
-
+db.init(function(result){
+        if(result){
+            console.dir(result);
+            console.log('ERROR in DATABASE Installation failed!!!');
+            process.exit();
+        }
+        else{
+            console.log('Successfuuly configured Database!!!');
+            process.exit();
+        }
+        
+    });
     
